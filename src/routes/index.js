c72158cd386067e2ee4996d0db4acd0e25aa90d9
Odannys de La Cruz <el_odanis321@hotmail.com>
2023-06-17 18:17:40 -0500

    Agregar rutas auth a index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express')
 const router = Router()
+const authRoutes = require('./auth.routes')
 const productsRoutes = require('./products.routes')
 const categoriesRoutes = require('./categories.routes')
 
+router.use('/api/v1/auth', authRoutes)
 router.use('/api/v1/products', productsRoutes)
 router.use('/api/v1/categories', categoriesRoutes)
 
